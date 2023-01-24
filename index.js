d89275f5561079efd37bae2f48455e356911436a
olivia-e-brown <olivia.el.brown@gmail.com>
2023-01-24 01:21:52 +0000

    Added Unsplash.js

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 import { OpenAI } from "./src/openai/OpenAI.js";
+import { Unsplash } from "./src/unsplash/Unsplash.js";
 import dotenv from 'dotenv';
 dotenv.config();
 
 // Creating a new instance of the OpenAI class and passing in the OPENAI_KEY environment variable
 const openAI = new OpenAI(process.env.OPENAI_KEY);
+const unsplash = new Unsplash(process.env.UNSPLASH_KEY);
 
-const prompt = 'write a blog post about paris?';
+const topic = 'NodeJs';
 const model = 'text-davinci-003';
 
 // Function to generate the prompt for the OpenAI API 
 // In the future, it will be moved to a helper class in the next code review
-const generatePrompt = (prompt) => {
-    return `Write an blog post about "${prompt}", it should in HTML format, include 5 unique points, using informative tone.`
+const generatePrompt = (topic) => {
+    return `Write an blog post about "${topic}", it should in HTML format, include 5 unique points, using informative tone.`
 };
 
 // Use the generateText method to generate text from the OpenAI API and passing the generated prompt, the model and max token value
-await openAI.generateText(generatePrompt(prompt), model, 800)
+await openAI.generateText(generatePrompt(topic), model, 800)
     .then(text => {
         // Logging the generated text to the console
         // In the future, this will be replaced to upload the returned blog text to a WordPress site using the WordPress REST API
@@ -25,5 +27,7 @@ await openAI.generateText(generatePrompt(prompt), model, 800)
         console.error(error);
     });
 
+// Use the getPhoto method to get a photo from the Usplash API by passing the type of responese expected and the photo search phrase
+await unsplash.getPhoto('file', 'coding on laptop');
 
 
---
+++ src/unsplash/Unsplash.js
@@ -0,0 +1,86 @@
+import { createApi } from 'unsplash-js';
+import logger from '../utils/logger.js';
+import fetch from 'node-fetch';
+import fs from 'fs';
+
+
+export class Unsplash {
+  constructor(accessKey) {
+    // Create an instance of the Unsplash API using the provided access key
+    this.unsplash = createApi({ accessKey, fetch });
+  }
+
+  async getPhoto(type, query, page = 1, per_page = 8, orientation = 'landscape' ) {
+    try {
+      // Log that a request is being sent to the Unsplash API
+      logger.info(`Sending request to Unsplash API with search phrase: ${query}`);
+
+       // Send a request to the Unsplash API to search for photos
+      const response  = await this.unsplash.search.getPhotos({
+        query,
+        page,
+        per_page,
+        orientation,
+      });
+
+      // Select a random photo from the response
+      const aRandomPhoto = response.response.results[Math.floor(Math.random() * 8)];
+      // Get the regular size photo url
+      const photoUrl = aRandomPhoto.urls.regular;
+      // Fetch the photo
+      const photo = await fetch(photoUrl);
+      // Get the photo buffer
+      const photoBuffer = await photo.arrayBuffer();
+      // Create caption for the photo - in Unsplash attribution style
+      const caption = `
+        <a style="text-decoration: none; cursor: default; pointer-events: none; color: inherit;">
+          Photo by
+        </a>
+        <a href="${aRandomPhoto.user.links.html}" rel="noopener ugc nofollow" target="_blank">
+          ${aRandomPhoto.user.name}
+        </a>
+        on
+        <a href="https://unsplash.com" rel="noopener ugc nofollow" target="_blank">
+          Unsplash
+        </a>
+      `;
+
+      // Log that a response has been received
+      logger.info(`Received response from Unsplash API`);
+
+      // Check the value of the "type" parameter and execute the corresponding code block
+      switch (type) {
+        case 'buffer':
+          // Convert the photo buffer to Uint8Array
+          const data = new Uint8Array(photoBuffer);
+          console.log(`${query}.jpg buffer ready`);
+          // Return an object containing the photo's buffer and attributes
+          return {
+            attributes: {
+              caption: caption,
+              title: query.toLowerCase(),
+              alt_text: `an image of ${query.toLowerCase()}`,
+            },
+            buffer: data
+          };
+        case 'file':
+          // Convert the photo buffer to a Buffer
+          const image = Buffer.from(photoBuffer);
+           // Create a file path for the photo
+          const filePath = `src/images/${query}.jpg`
+          // Write the photo to the file system
+          await fs.promises.writeFile(filePath, image);
+          console.log(caption);
+          console.log(`${query}.jpg saved`);
+          break;
+        default:
+          console.log(`Invalid type: ${type}`);
+          return null;
+      }
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
+}
